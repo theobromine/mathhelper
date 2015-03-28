2d366dcde16784fa8aa46b422456d903e1324a53
colaalex <colaalex4r@mail.ru>
2015-03-28 20:30:27 +0300

    add pretty printing
    there's only version with sympy. added pretty printing.

--- main.py
+++ main.py
@@ -1,117 +1,133 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-from kivy.properties import ObjectProperty
+from sympy import *
 from math import sqrt
 
 
 class MainScreen(Screen):
-
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-
-        layout = FloatLayout(size=(300, 300))
-        button1 = Button(text="Квадратные уравнения", size_hint=(.5, .5), pos_hint={"left": 2, 'top': 2})
-        button1.bind(on_press=self.sqeq)
-        layout.add_widget(button1)
-        # button2 = Button(text="СЛАУ", size_hint=(.5, .5), pos_hint={"left": 1, 'top': 2})
-        # button2.bind(on_press=self.slau)
-        # button3 = Button(text="Теорема Пифагора", size_hint=(.5, .5), pos_hint={"left": 2, 'top': 1})
-        # button3.bind(on_press=self.pifa)
-        # button4 = Button(text="Теорема Герона", size_hint=(.5, .5), pos_hint={"left": 2, 'top': 2})
-        # button4.bind(on_press=self.geron)
-
-        # layout.add_widget(button2)
-        # layout.add_widget(button3)
-        # layout.add_widget(button4)
-
-    def sqeq(self, *args):
-        self.manager.current = 'SquareEquals'
-    # coming soon:
-    # def slau(self, *args):
-        # self.manager.current = 'SLAU'
-
-    # def pifa(self, *args):
-        # self.manager.current = 'Pifagor'
-
-    # def geron(self, *args):
-        # self.manager.current = 'Geron'
+    pass
 
 
 class SquareEquals(Screen):
-
-    def count(self, a, b, c):
-        a = int(a)
-        b = int(b)
-        c = int(c)
-        global D, x1, x2
-        D = 0
-        x1 = 0
-        x2 = 0
-        D = b**2-4*a*c
-        if D >= 0:
-            x1 = ((-1 * b) + sqrt(D))/(2*a)
-            x2 = ((-1 * b) - sqrt(D))/(2*a)
-            return x1, x2, D
-            ti4.text = x1
-            ti5.text = x2
-            ti6.text = D
+    def math(self, instance):
+        a = int(self.a.text)
+        b = int(self.b.text)
+        c = int(self.c.text)
+        x = Symbol('x')
+        dis = int(self.b.text)**2 - 4 * int(self.a.text) * int(self.c.text)
+        if dis >= 0:
+            a = solve((a*(x**2))+(b*x)+c, x)
+            x1 = a[0]
+            x2 = a[1]
+            self.result.text = pretty(x1) + '\n' + pretty(x2)
         else:
-            print("D отрицательный")
-            return 0
+            self.result.text = 'D negative'
 
-    def __init__(self, **kwargs):
-        super(SquareEquals, self).__init__(**kwargs)
+class MathFunction2(Screen):
+    pass
 
-        layout = FloatLayout(size=(100, 100))
-        ti = TextInput(multiline=False, text='1', size_hint=(.2, .2), pos_hint={"left": 1, 'top': 1})
-        ti2 = TextInput(multiline=False, text='1', size_hint=(.2, .2), pos_hint={"left": 3, 'top': 1})
-        ti3 = TextInput(multiline=False, text='1', size_hint=(.2, .2), pos_hint={"left": 5, 'top': 1})
-        layout.add_widget(ti)
-        layout.add_widget(ti2)
-        layout.add_widget(ti3)
-        b1 = Button(text='Решить!', size_hint=(1, .2), pos_hint={"left": 1, 'top': 2})
-        b1.bind(on_press=self.count(ti.text, ti2.text, ti3.text))
-        global ti4, ti5, ti6
-        ti4 = TextInput(multiline=False, size_hint=(.2, .2), pos_hint={"left": 2, 'top': 3})
-        ti5 = TextInput(multiline=False, size_hint=(.2, .2), pos_hint={"left": 2, 'top': 4})
-        ti6 = TextInput(multiline=False, size_hint=(.2, .2), pos_hint={"left": 2, 'top': 5})
-        l1 = Label(text='x[sup]2[/sup] + ', size_hint=(.125, .2), pos_hint={"left": 2, 'top': 1})
-        l2 = Label(text='x + ', size_hint=(.125, .2), pos_hint={"left": 4, 'top': 1})
-        l3 = Label(text='= 0', size_hint=(.15, .2), pos_hint={"left": 6, 'top': 1})
-        l4 = Label(text='x1 = ', size_hint=(.2, .2), pos_hint={"left": 1, 'top': 1})
-        l5 = Label(text='x2 = ')
-        l6 = Label(text='D = ')
-        bex = Button(text='Назад')
-        bex.bind(on_press=self.menu)
-        layout.add_widget(ti4)
-        layout.add_widget(ti5)
-        layout.add_widget(ti6)
-        layout.add_widget(b1)
-        layout.add_widget(bex)
-        layout.add_widget(l1)
-        layout.add_widget(l2)
-        layout.add_widget(l3)
-        layout.add_widget(l4)
-        layout.add_widget(l5)
-        layout.add_widget(l6)
 
-    def menu(self, *args):
-        self.manager.current = 'Main'
+class MathFunction3(Screen):
+    pass
+
+
+class MyScreenManager(ScreenManager):
+    pass
+
+
+main_screen = Builder.load_string('''
+MyScreenManager:
+    MainScreen:
+    SquareEquals:
+    MathFunction2:
+    MathFunction3:
+<MainScreen>:
+    name: 'MainScreen'
+    BoxLayout:
+        orientation: 'vertical'
+        Button:
+            text: 'Square equals'
+            size_hint: (.5, .5)
+            on_release: app.root.current = 'SquareEquals'
+        Button:
+            text: 'Math function # 2'
+            size_hint: (.5, .5)
+            on_release: app.root.current = 'MathFunction2'
+        Button:
+            text: 'Math function # 3'
+            size_hint: (.5, .5)
+            on_release: app.root.current = 'MathFunction3'
+<SquareEquals>:
+    a: _a
+    b: _b
+    c: _c
+    result: _result
+    name: 'SquareEquals'
+    BoxLayout:
+        orientation: 'vertical'
+        size_hint: (1,1)
+        Label:
+            text: 'Square Equals'
+            font_size: 30
+            size_hint: (.3,.1)
+        GridLayout:
+            rows: 2
+            orientation: 'vertical'
+            TextInput:
+                id: _a
+                multiline: 'false'
+            TextInput:
+                id: _b
+                multiline: 'false'
+            TextInput:
+                id: _c
+                multiline: 'false'
+            Button:
+                text: 'Do the math'
+                on_release: root.math(*args)
+            Button:
+                id: _result
+                text: ''
+            BoxLayout:
+                Button:
+                    text: 'Go to main screen'
+                    font_size: 30
+                    on_release: app.root.current = 'MainScreen'
+<MathFunction2>:
+    name: 'MathFunction2'
+    BoxLayout:
+        orientation: 'vertical'
+        Label:
+            text: 'Math Function 2'
+            font_size: 30
+        BoxLayout:
+            Button:
+                text: 'Go to main screen'
+                font_size: 30
+                on_release: app.root.current = 'MainScreen'
+<MathFunction3>:
+    name: 'MathFunction3'
+    BoxLayout:
+        orientation: 'vertical'
+        Label:
+            text: 'Math Function 3'
+            font_size: 30
+        BoxLayout:
+            Button:
+                text: 'Go to main screen'
+                font_size: 30
+                on_release: app.root.current = 'MainScreen'
+''')
 
 
 class MyApp(App):
-
     def build(self):
-        sm = ScreenManager()
-        s1 = MainScreen(name='Main')
-        s2 = SquareEquals(name='SquareEquals')
-        sm.add_widget(s1)
-        sm.add_widget(s2)
-        return sm
+        return main_screen
 
 
 if __name__ == '__main__':
